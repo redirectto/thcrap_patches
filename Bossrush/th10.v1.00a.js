264bbcca546baf6b5295bdaaca50267571fff6e9
redirectto <60912584+redirectto@users.noreply.github.com>
2021-07-30 08:37:15 +0200

    Added Bossrush and Bossonly patches for MoF

---
+++ Bossrush/th10.v1.00a.js
@@ -0,0 +1,46 @@
+{
+	"binhacks": {
+		"scorefile_ptr": {
+			"addr": [
+				"Rx2ae79",
+				"Rx2b33b"
+			],
+			"code": "<0x400300>"
+		},
+		"scorefile_static_data": {
+			"addr": "Rx00300",
+			"code": "73 63 6F 72 65 74 68 31 30 5F 42 6F 73 73 72 75 73 68 5F 72 65 64 69 2E 64 61 74"
+		},
+		"replay_printf_ptr": {
+			"addr": [
+				"Rx29bce",
+				"Rx2a249"
+			],
+			"code": "<0x400330>"
+		},
+		"replay_printf_static_data": {
+			"addr": "Rx00330",
+			"code": "72 65 70 6C 61 79 5F 42 6F 73 73 72 75 73 68 5F 72 65 64 69 2F 25 73"
+		},
+		"replay_folder_ptr": {
+			"addr": [
+				"Rx29bbf",
+				"Rx316b0",
+				"Rx316ba",
+				"Rx31708"
+			],
+			"code": "<0x400360>"
+		},
+		"replay_folder_static_data": {
+			"addr": "Rx00360",
+			"code": "72 65 70 6C 61 79 5F 42 6F 73 73 72 75 73 68 5F 72 65 64 69"
+		},
+		"disable_continues_ptr": {
+            "addr": "Rx23910",
+			"code": "E9[codecave:disable_continues]90"
+		}
+	},
+	"codecaves":{
+		"disable_continues": "0F84[0x423A0A] 837D2400 7507 C74524 02000000 8B4524 E9[0x423919]"
+	}
+}
