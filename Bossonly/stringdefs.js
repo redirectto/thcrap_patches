976e2dba8d432bddd2ca6fb0b83d292a4bbced38
redirectto <60912584+redirectto@users.noreply.github.com>
2021-09-16 13:58:40 +0200

    Added stringdefs to modify the title of the main window to both bossonly and bossrush

---
+++ Bossonly/stringdefs.js
@@ -0,0 +1,16 @@
+{
+    // "th06": "Touhou Scarlet Devil Land ~ The Embodiment of Scarlet Devil (Bossonly)",
+    // "th07": "Touhou Eastern Ghostly Dream ~ Perfect Cherry Blossom (Bossonly)",
+    // "th08": "Touhou Eternal Night Vignette ~ Imperishable Night (Bossonly)",
+    "th10": "Touhou Eastern Wind God Chronicles ~ Mountain of Faith (Bossonly)",
+    // "alcostg": "Twilight Bar Room ~ Uwabami Breakers (Bossonly)",
+    "th11": "Touhou Earth Spirit Palace ~ Subterranean Animism (Bossonly)",
+    "th12": "Touhou Star Lotus Ship ~ Undefined Fantastic Object (Bossonly)",
+    // "th128": "Great Fairy Wars ~ Touhou Three Fairies (Bossonly)",
+    "th13": "Touhou Divine Spirit Mausoleum ~ Ten Desires (Bossonly)",
+    "th14": "Touhou Shining Needle Castle ~ Double Dealing Character (Bossonly)",
+    "th15": "Touhou Eastern Legend of the Ultramarine Orb ~ Legacy of Lunatic Kingdom (Bossonly)",
+    "th16": "Touhou Eastern Jade Scepter of the Skies ~ Hidden Star in Four Seasons (Bossonly)",
+    "th17": "Touhou Eastern Beast in Oni's Shape ~ Wily Beast and Weakest Creature (Bossonly)",
+    "th18": "Touhou Rainbow Dragon Cavern ~ Unconnected Marketeers (Bossonly)"
+}
---
+++ Bossrush/stringdefs.js
@@ -0,0 +1,16 @@
+{
+    // "th06": "Touhou Scarlet Devil Land ~ The Embodiment of Scarlet Devil (Bossrush)",
+    // "th07": "Touhou Eastern Ghostly Dream ~ Perfect Cherry Blossom (Bossrush)",
+    // "th08": "Touhou Eternal Night Vignette ~ Imperishable Night (Bossrush)",
+    "th10": "Touhou Eastern Wind God Chronicles ~ Mountain of Faith (Bossrush)",
+    // "alcostg": "Twilight Bar Room ~ Uwabami Breakers (Bossrush)",
+    "th11": "Touhou Earth Spirit Palace ~ Subterranean Animism (Bossrush)",
+    "th12": "Touhou Star Lotus Ship ~ Undefined Fantastic Object (Bossrush)",
+    // "th128": "Great Fairy Wars ~ Touhou Three Fairies (Bossrush)",
+    "th13": "Touhou Divine Spirit Mausoleum ~ Ten Desires (Bossrush)",
+    "th14": "Touhou Shining Needle Castle ~ Double Dealing Character (Bossrush)",
+    "th15": "Touhou Eastern Legend of the Ultramarine Orb ~ Legacy of Lunatic Kingdom (Bossrush)",
+    "th16": "Touhou Eastern Jade Scepter of the Skies ~ Hidden Star in Four Seasons (Bossrush)",
+    "th17": "Touhou Eastern Beast in Oni's Shape ~ Wily Beast and Weakest Creature (Bossrush)",
+    "th18": "Touhou Rainbow Dragon Cavern ~ Unconnected Marketeers (Bossrush)"
+}
