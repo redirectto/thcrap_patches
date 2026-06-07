f3d536b1c05a98e3566b491a9a3ef52c43a0bba4
redirectto <er.redirectto@gmail.com>
2026-06-08 00:58:29 +0200

    Updated patch.js Added BossDisplayFixes as a dependency Updated title on thpatch, save file instead of save folder

--- BraviOCOkina_th16ufos/patch.js
+++ BraviOCOkina_th16ufos/patch.js
@@ -5,11 +5,12 @@
     "nmlgc/western_name_order",
     "thpatch/lang_en",
     "Priw8/th16ufos",
+    "Bravi/BossDisplayFixes",
     "Bravi/BraviOCOkina"
   ],
   "id": "BraviOCOkina_th16ufos",
   "servers": [
     "https://mirrors.thpatch.net/redirectto/BraviOCOkina_th16ufos/"
   ],
-  "title": "UFOs on BraviOCOkina patch (HSiFS) Save folder = BraviOCO_UFOs"
+  "title": "UFOs on BraviOCOkina patch (HSiFS) Save file = BraviOCO_UFOs.dat"
 }
